da7ad1ef7b0867714f03f3b44970c483c04392fe
Liam <82466595+LiamChau@users.noreply.github.com>
2026-04-15 05:38:28 +0800

    Add Spotify shuffle fix for Quantumult X
    This script enhances the request-header cleaner for Spotify shuffle in Quantumult X by removing common caching headers and setting no-cache directives.

---
+++ spotify-shuffle-fix.js
@@ -0,0 +1,23 @@
+/**
+ * Spotify shuffle fix for Quantumult X
+ * Enhanced request-header cleaner
+ * Best used with Amlabort spot-proto-eevee.js
+ */
+
+let headers = $request.headers || {};
+
+// 清理所有常见缓存头
+delete headers["If-None-Match"];
+delete headers["if-none-match"];
+delete headers["ETag"];
+delete headers["etag"];
+delete headers["Cache-Control"];
+delete headers["cache-control"];
+delete headers["If-Modified-Since"];
+delete headers["if-modified-since"];
+
+// 某些版本会缓存 customize 配置
+headers["Cache-Control"] = "no-cache";
+headers["Pragma"] = "no-cache";
+
+$done({ headers });
