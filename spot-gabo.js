0611ec230a2707a4678f00f1736142b3e4e2a4de
Liam <82466595+LiamChau@users.noreply.github.com>
2026-04-16 18:07:25 +0800

    Update spot-gabo.js

--- spot-gabo.js
+++ spot-gabo.js
@@ -10,11 +10,15 @@ $done({
   response: {
     status: 200,
     headers: {
-      "cache-control": "no-cache",
-      "content-type": "application/json",
-      "date": new Date().toUTCString(),
-      "strict-transport-security": "max-age=31536000",
-      "x-content-type-options": "nosniff"
+    "cache-control": "no-cache",
+    "content-length": "0",
+    "server-timing": "edge;dur=666",
+    "strict-transport-security": "max-age=31536000",
+    "x-content-type-options": "nosniff",
+    "alt-svc": 'h3=":443"; ma=2592000,h3-29=":443"; ma=2592000',
+    "date": new Date().toUTCString(),
+    "server": "envoy",
+    "via": "HTTP/2 edgeproxy, 1.1 google"
     },
     body: ""
   }
